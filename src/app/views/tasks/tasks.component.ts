23bb80e4615ed21187d07230823fec9ba28f1b43
Agnes <agnesft5@gmail.com>
2020-04-25 02:38:16 +0200

    Media queries added

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -8,14 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class TasksComponent implements OnInit {
 
   sideBarOpened: boolean = false;
+  showCategories: boolean = false;
+  showItems: boolean = false;
 
   constructor() { }
 
   openSideBar(){
     if(this.sideBarOpened == false){
-      this.sideBarOpened = true
+      this.sideBarOpened = true;
+      this.showItems = true;
+      setTimeout(() => {
+        this.showCategories = true
+      }, 1000);
     }else if (this.sideBarOpened == true){
-      this.sideBarOpened = false
+      this.sideBarOpened = false;
+      this.showCategories = false;
+      setTimeout(()=>{
+        this.showItems = false;
+      }, 1750)
     }
   }
 
